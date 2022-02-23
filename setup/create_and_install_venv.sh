d413cb3b07020e34d3a6f26fc99c16552c330edb
Cheil <cheil@106-104-130-77.adsl.static.seed.net.tw>
2022-02-23 11:29:43 +0100

    fix: division feeds new: sh scripts

---
+++ setup/create_and_install_venv.sh
@@ -0,0 +1,10 @@
+
+# To be executed at root project
+# Install venv
+python3 -m pip install virtualenv
+# Create venv
+python3 -m virtualenv feeds-cheil-venv 
+# Activate
+source feeds-cheil-venv/bin/activate
+
+pip install -r setup/requirements.txt
