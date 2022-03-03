414732836b706a011e3e2af9955974e04bf3f021
Cheil <cheil@MacBook-Pro-de-Cheil.local>
2022-03-03 10:19:34 +0100

    new: standarized categories

--- src/scripts/execution/pyscripts/addDivision.py
+++ src/scripts/execution/pyscripts/addDivision.py
@@ -1,20 +1,17 @@
 import pandas as pd
 from config import result_path
-from Trafficking_Cheil import DivisionByCategory
+from Trafficking_Cheil import StandarizedCategory
 
 providers= ['AWIN','Facebook','KuantoKusta','Criteo']
 
-tags_dict= {'KuantoKusta': ('division','category'), 
-            'Facebook': ('g:division','g:google_product_category'),
-            'Criteo': ('g:division','g:google_product_category')}
+tags_dict= {'KuantoKusta': 'reference'}
 
 if __name__ == '__main__':
     
-    get_division= DivisionByCategory()
+    get_new_categories= StandarizedCategory()
 
     for provider in providers:
         df = pd.read_csv(f'{result_path}/{provider}Feed.csv')
-        tags = tags_dict[provider] if provider in tags_dict else ('g:division','g:product_type')
-        df = get_division.add_division_to_df(df,*tags)
+        df = get_new_categories.add_standarized_cats(df,tags_dict[provider] if provider in tags_dict else 'g:id')
         df.to_csv(f'{result_path}/division/{provider}FeedDivision_Cheil.csv',index=False)
 
