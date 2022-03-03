b7ae3b2b2e1290e78ded016bbdcb974f15e88b58
Cheil <cheil@MacBook-Pro-de-Cheil.local>
2022-03-03 11:40:08 +0100

    new: shell scripts for installing pckg

--- setup/shscripts/create_and_install_venv.sh
+++ setup/shscripts/create_and_install_venv.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-
+#!/bin/bash
 # To be executed at root project
 # Install venv
 python3 -m pip install virtualenv
@@ -7,7 +7,7 @@ python3 -m pip install virtualenv
 python3 -m virtualenv feeds-cheil-venv 
 # Activate
 source feeds-cheil-venv/bin/activate
-
+# Install requirements
 pip install -r setup/requirements.txt
-
+# Source the environmental files
 source .env
